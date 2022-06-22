f0cdc361e03d2e73e3c9a926a6f29e6779e1e2e6
Peter Berkecz <berkecz.peter@gmail.com>
2022-06-22 16:12:13 +0200

    ci: Fixing cron, added linking

---
+++ linking.sh
@@ -0,0 +1,6 @@
+ln huspacy/hu_core_news_lg/README.md site/docs/lg/index.md &&
+ln huspacy/hu_core_news_lg/CHANGELOG.md site/docs/lg/changelog.md &&
+ln huspacy/hu_core_news_trf/README.md site/docs/trf/index.md &&
+ln huspacy/hu_core_news_trf/CHANGELOG.md site/docs/trf/changelog.md &&
+ln huspacy/hu_vectors/README.md site/docs/lg_vectors/index.md &&
+ln huspacy/hu_vectors/CHANGELOG.md site/docs/lg_vectors/changelog.md
