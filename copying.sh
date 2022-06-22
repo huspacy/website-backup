92ddf5576d7e86630833aedce6092bc7378b2bf1
Peter Berkecz <berkecz.peter@gmail.com>
2022-06-22 17:11:53 +0200

    fix: Fixed copying

--- copying.sh
+++ copying.sh
@@ -1,3 +1,4 @@
+mkdir -p {site/docs/lg,site/docs/trf,site/docs/lg_vectors}
 cp huspacy/hu_core_news_lg/README.md site/docs/lg/index.md 
 cp huspacy/hu_core_news_lg/CHANGELOG.md site/docs/lg/changelog.md 
 cp huspacy/hu_core_news_trf/README.md site/docs/trf/index.md 
