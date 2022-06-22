5e49b4ec087f228c0d80892f8dca536137fb844c
Peter Berkecz <berkecz.peter@gmail.com>
2022-06-22 17:02:22 +0200

    ci: Changed linking to copying

---
+++ copying.sh
@@ -0,0 +1,6 @@
+cp huspacy/hu_core_news_lg/README.md site/docs/lg/index.md 
+cp huspacy/hu_core_news_lg/CHANGELOG.md site/docs/lg/changelog.md 
+cp huspacy/hu_core_news_trf/README.md site/docs/trf/index.md 
+cp huspacy/hu_core_news_trf/CHANGELOG.md site/docs/trf/changelog.md 
+cp huspacy/hu_vectors/README.md site/docs/lg_vectors/index.md 
+cp huspacy/hu_vectors/CHANGELOG.md site/docs/lg_vectors/changelog.md
