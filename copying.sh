df68c27e3c99b402657634ab1f27819d04991e4a
Peter Berkecz <berkecz.peter@gmail.com>
2022-08-12 11:48:12 +0200

    fix: changed lg_vector folder
    Since in the main repo the large vector has been refactorized, the website build failed

--- copying.sh
+++ copying.sh
@@ -3,5 +3,5 @@ cp huspacy/hu_core_news_lg/README.md site/docs/lg/index.md
 cp huspacy/hu_core_news_lg/CHANGELOG.md site/docs/lg/changelog.md 
 cp huspacy/hu_core_news_trf/README.md site/docs/trf/index.md 
 cp huspacy/hu_core_news_trf/CHANGELOG.md site/docs/trf/changelog.md 
-cp huspacy/hu_vectors/README.md site/docs/lg_vectors/index.md 
-cp huspacy/hu_vectors/CHANGELOG.md site/docs/lg_vectors/changelog.md
+cp huspacy/hu_vectors_web_lg/README.md site/docs/lg_vectors/index.md 
+cp huspacy/hu_vectors_web_lg/CHANGELOG.md site/docs/lg_vectors/changelog.md
