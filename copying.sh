5efcf0b7ea2851a32fa7ce626066bdeaebae63fb
György Orosz <gyorgy@orosz.link>
2022-10-12 13:31:20 +0200

    Update copying.sh

--- copying.sh
+++ copying.sh
@@ -1,7 +1,11 @@
-mkdir -p {site/docs/lg,site/docs/trf,site/docs/lg_vectors}
+mkdir -p {site/docs/lg,site/docs/md,site/docs/trf,site/docs/lg_vectors,site/docs/md_vectors}
+cp huspacy/hu_core_news_md/README.md site/docs/md/index.md 
+cp huspacy/hu_core_news_md/CHANGELOG.md site/docs/md/changelog.md 
 cp huspacy/hu_core_news_lg/README.md site/docs/lg/index.md 
 cp huspacy/hu_core_news_lg/CHANGELOG.md site/docs/lg/changelog.md 
 cp huspacy/hu_core_news_trf/README.md site/docs/trf/index.md 
 cp huspacy/hu_core_news_trf/CHANGELOG.md site/docs/trf/changelog.md 
 cp huspacy/hu_vectors_web_lg/README.md site/docs/lg_vectors/index.md 
 cp huspacy/hu_vectors_web_lg/CHANGELOG.md site/docs/lg_vectors/changelog.md
+cp huspacy/hu_vectors_web_md/README.md site/docs/md_vectors/index.md 
+cp huspacy/hu_vectors_web_md/CHANGELOG.md site/docs/md_vectors/changelog.md
